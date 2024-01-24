55cd5f6ebeed833653781f754737e2bf0214547f
Rohit Kumar <113338596+Rohitk131@users.noreply.github.com>
2024-01-24 23:15:23 +0530

    Update Trending.jsx

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -15,20 +15,22 @@ const Trending = () => {
   return (
     <div className="container mx-auto mt-8">
       <h1 className="text-4xl font-bold mb-4 ml-10">Explore all <span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-red-500'>Trending Cryptocurrencies</span></h1>
-      {trendingCryptos.map((crypto, index) => (
-        <div key={crypto.item.id} className="bg-white p-4 rounded-2xl shadow-2xl mb-4 flex items-center transition-transform transform hover:scale-105">
-          <span className="text-xl font-semibold mr-2">{index + 1}.</span>
-          <img src={crypto.item.small} alt={crypto.item.name} className="w-10 h-10 mr-2 rounded-full" />
-          <div>
-            <h2 className="text-xl font-semibold mb-2">{crypto.item.name}</h2>
-            <p className="text-gray-700">
-              Symbol: {crypto.item.symbol}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              Market Cap Rank: {crypto.item.market_cap_rank}
-            </p>
+      <div className='m-10'>
+        {trendingCryptos.map((crypto, index) => (
+          <div key={crypto.item.id} className="bg-white p-4 rounded-2xl shadow-2xl mb-4 flex items-center transition-transform transform hover:scale-105">
+            <span className="text-xl font-semibold mr-2">{index + 1}.</span>
+            <img src={crypto.item.small} alt={crypto.item.name} className="w-10 h-10 mr-2 rounded-full" />
+            <div>
+              <h2 className="text-xl font-semibold mb-2">{crypto.item.name}</h2>
+              <p className="text-gray-700">
+                Symbol: {crypto.item.symbol}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Market Cap Rank: {crypto.item.market_cap_rank}
+              </p>
+            </div>
+            <p className="text-blue-500 ml-auto text-2xl font-semibold" dangerouslySetInnerHTML={{__html: crypto.item.data.price}}/>
           </div>
-          <p className="text-blue-500 ml-auto text-2xl font-semibold">{crypto.item.data.price}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
