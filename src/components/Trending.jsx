84a60a4573bd98965bf410b9fd6ccaa6fc923e66
rohitk131 <rohitkumar13103@gmail.com>
2024-01-24 16:06:34 +0530

    Trending page layout changed

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -14,16 +14,21 @@ const Trending = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <h1 className="text-4xl font-bold mb-4">Trending Cryptocurrencies</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {trendingCryptos.map(crypto => (
-    <div key={crypto.item.id} className="bg-white p-4 rounded shadow-md">
-        <h2 className="text-xl font-semibold mb-2">{crypto.item.name}</h2>
-        <p className="text-gray-700">Symbol: {crypto.item.symbol}</p>
-        <p className="text-gray-700">Market Cap Rank: {crypto.item.market_cap_rank}</p>
-    </div>
-    ))}
-      </div>
+      <h1 className="text-4xl font-bold mb-4">Explore all <span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-red-500'>Trending Cryptocurrencies</span></h1>
+      {trendingCryptos.map((crypto, index) => (
+        <div key={crypto.item.id} className="bg-white p-4 rounded shadow-md mb-4 flex items-center">
+          <span className="text-xl font-semibold mr-2">{index + 1}.</span>
+          <img src={crypto.item.small} alt={crypto.item.name} className="w-10 h-10 mr-2 rounded-full" />
+          <div>
+            <h2 className="text-xl font-semibold mb-2">{crypto.item.name}</h2>
+            <p className="text-gray-700">
+              Symbol: {crypto.item.symbol}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Market Cap Rank: {crypto.item.market_cap_rank}
+            </p>
+          </div>
+          <p className="text-blue-500 ml-auto text-2xl font-semibold">{crypto.item.data.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
